1ac4a3f375f6c8e701295d866adb7021d0b20321
Minee <78191030+ThaksinMuansuk@users.noreply.github.com>
2023-05-14 04:12:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+function toggle1() {
+    var data = document.getElementById("data1");
+    var fafaplus = document.getElementById("fa-plus");
+    var fafaminus = document.getElementById("fa-minus");
+    if (data.style.display === "none") {
+        data.style.display = "block";
+        fafaminus.style.display = "block";
+        fafaplus.style.display = "none";
+        // data.innerHTML = "นี่คือข้อมูล";
+    } else {
+        data.style.display = "none"; 
+        fafaminus.style.display = "none";
+        fafaplus.style.display = "block";
+        // data.innerHTML = "";
+    }
+}
+
+function toggle2() {
+    var data = document.getElementById("data2");
+    var fafaplus = document.getElementById("fa-plus2");
+    var fafaminus = document.getElementById("fa-minus2");
+    if (data.style.display === "none") {
+        data.style.display = "block";
+        fafaminus.style.display = "block";
+        fafaplus.style.display = "none";
+        // data.innerHTML = "นี่คือข้อมูล";
+    } else {
+        data.style.display = "none"; 
+        fafaminus.style.display = "none";
+        fafaplus.style.display = "block";
+        // data.innerHTML = "";
+    }
+}
+
+function toggle3() {
+    var data = document.getElementById("data3");
+    var fafaplus = document.getElementById("fa-plus3");
+    var fafaminus = document.getElementById("fa-minus3");
+    if (data.style.display === "none") {
+        data.style.display = "block";
+        fafaminus.style.display = "block";
+        fafaplus.style.display = "none";
+        // data.innerHTML = "นี่คือข้อมูล";
+    } else {
+        data.style.display = "none"; 
+        fafaminus.style.display = "none";
+        fafaplus.style.display = "block";
+        // data.innerHTML = "";
+    }
+}
+
+function toggle4() {
+    var data = document.getElementById("data4");
+    var fafaplus = document.getElementById("fa-plus4");
+    var fafaminus = document.getElementById("fa-minus4");
+    if (data.style.display === "none") {
+        data.style.display = "block";
+        fafaminus.style.display = "block";
+        fafaplus.style.display = "none";
+        // data.innerHTML = "นี่คือข้อมูล";
+    } else {
+        data.style.display = "none"; 
+        fafaminus.style.display = "none";
+        fafaplus.style.display = "block";
+        // data.innerHTML = "";
+    }
+}
+
+function toggle5() {
+    var data = document.getElementById("data5");
+    var fafaplus = document.getElementById("fa-plus5");
+    var fafaminus = document.getElementById("fa-minus5");
+    if (data.style.display === "none") {
+        data.style.display = "block";
+        fafaminus.style.display = "block";
+        fafaplus.style.display = "none";
+        // data.innerHTML = "นี่คือข้อมูล";
+    } else {
+        data.style.display = "none"; 
+        fafaminus.style.display = "none";
+        fafaplus.style.display = "block";
+        // data.innerHTML = "";
+    }
+}
+
+function toggle6() {
+    var data = document.getElementById("data6");
+    var fafaplus = document.getElementById("fa-plus6");
+    var fafaminus = document.getElementById("fa-minus6");
+    if (data.style.display === "none") {
+        data.style.display = "block";
+        fafaminus.style.display = "block";
+        fafaplus.style.display = "none";
+        // data.innerHTML = "นี่คือข้อมูล";
+    } else {
+        data.style.display = "none"; 
+        fafaminus.style.display = "none";
+        fafaplus.style.display = "block";
+        // data.innerHTML = "";
+    }
+}
